d0d5ec7af58efdc344029641d2a7dd8ceb545a36
Kat Sullivan <kat.sullivan@wpp.com>
2026-03-18 12:23:18 +0000

    trying spot with arm

--- source/isaaclab_assets/isaaclab_assets/robots/spot.py
+++ source/isaaclab_assets/isaaclab_assets/robots/spot.py
@@ -180,3 +180,52 @@ SPOT_CFG = ArticulationCfg(
     },
 )
 """Configuration for the Boston Dynamics Spot robot."""
+
+SPOT_CFG_ARM = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f"/home/wpp/isaacsim/IsaacLab/SPOT_VENICE_ARM_RL_V1/spot_with_arm.usd",
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=True, solver_position_iteration_count=4, solver_velocity_iteration_count=0
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.5),
+        joint_pos={
+            "[fh]l_hx": 0.1,  # all left hip_x
+            "[fh]r_hx": -0.1,  # all right hip_x
+            "f[rl]_hy": 0.9,  # front hip_y
+            "h[rl]_hy": 1.1,  # hind hip_y
+            ".*_kn": -1.5,  # all knees
+        },
+        joint_vel={".*": 0.0},
+    ),
+    actuators={
+        "spot_hip": DelayedPDActuatorCfg(
+            joint_names_expr=[".*_h[xy]"],
+            effort_limit=45.0,
+            stiffness=60.0,
+            damping=1.5,
+            min_delay=0,  # physics time steps (min: 2.0*0=0.0ms)
+            max_delay=4,  # physics time steps (max: 2.0*4=8.0ms)
+        ),
+        "spot_knee": RemotizedPDActuatorCfg(
+            joint_names_expr=[".*_kn"],
+            joint_parameter_lookup=joint_parameter_lookup,
+            effort_limit=None,  # torque limits are handled based experimental data (`RemotizedPDActuatorCfg.data`)
+            stiffness=60.0,
+            damping=1.5,
+            min_delay=0,  # physics time steps (min: 2.0*0=0.0ms)
+            max_delay=4,  # physics time steps (max: 2.0*4=8.0ms)
+        ),
+    },
+)
